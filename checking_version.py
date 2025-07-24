2e26b0264dbe8f3a3f49c596365f8daf6efed0d2
Tariq Amin <tariqxa@gmail.com>
2025-07-24 19:45:48 +0100

    cleaning of data completed

---
+++ checking_version.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("Seaborn version:", sns.__version__)
+print("Matplotlib version:", plt.__version__)
